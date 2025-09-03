42a2c2df1301917a868ffa970935fbdcc8f21f01
nikhil09jeetnkj <230241935+nikhil09jeetnkj@users.noreply.github.com>
2025-09-04 02:44:25 +0530

    Initial project upload.

---
+++ pages/_document.js
@@ -0,0 +1,22 @@
+
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta name="theme-color" content="#0b1020" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
